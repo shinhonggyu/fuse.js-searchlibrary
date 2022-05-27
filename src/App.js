d220e748ff386d5799c8ef02f51fc80a060754d7
shinhonggyu <zowoz8819@gmail.com>
2022-05-28 00:29:08 +0900

    Step 2: Creating a new Fuse search instance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import characters from "./characters.json";
+import Fuse from "fuse.js";
 
-import characters from './characters.json';
+const fuse = new Fuse(characters, {
+  keys: ["name", "company", "species"],
+  includeScore: true,
+});
+
+const results = fuse.search("bender");
+const characterResults = results.map((character) => character.item);
 
 function App() {
   return (
     <>
-
       <header className="App-header">
         <div className="container">
           <h1>Futurama Characters</h1>
@@ -15,26 +22,29 @@ function App() {
 
       <main className="container">
         <ul className="characters">
-          {characters.map(character => {
+          {characterResults.map((character) => {
             const { name, company, species, thumb } = character;
             return (
               <li key={name} className="character">
-                <span className="character-thumb" style={{
-                  backgroundImage: `url(${thumb})`
-                }} />
+                <span
+                  className="character-thumb"
+                  style={{
+                    backgroundImage: `url(${thumb})`,
+                  }}
+                />
                 <ul className="character-meta">
                   <li>
-                    <strong>Name:</strong> { name }
+                    <strong>Name:</strong> {name}
                   </li>
                   <li>
-                    <strong>Company:</strong> { company }
+                    <strong>Company:</strong> {company}
                   </li>
                   <li>
-                    <strong>Species:</strong> { species }
+                    <strong>Species:</strong> {species}
                   </li>
                 </ul>
               </li>
-            )
+            );
           })}
         </ul>
         <aside>
@@ -48,7 +58,8 @@ function App() {
       <footer>
         <div className="container">
           <p>
-            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a> via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
+            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a>{" "}
+            via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
           </p>
         </div>
       </footer>
